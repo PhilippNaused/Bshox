e113e1d8e83467e4681d5a354dd70d6efa82d3db
PhilippNaused <philipp.naused@t-online.de>
2025-07-29 19:21:17 +0200

    add samples

---
+++ samples/Exampe1.cs
@@ -0,0 +1,18 @@
+using Bshox;
+using Bshox.Attributes;
+
+[BshoxSerializer(typeof(int), typeof(string))]
+partial class Example1;
+
+partial class Tests
+{
+    [Test]
+    public async Task Example1Test()
+    {
+        byte[] data = Example1.Int32.Serialize(42);
+        await Assert.That(data).IsEquivalentTo(new byte[] { 42 });
+
+        data = Example1.String.Serialize("Hello, World!");
+        await Assert.That(data).IsEquivalentTo("\rHello, World!"u8.ToArray());
+    }
+}
