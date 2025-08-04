680a2f843f1ac8a804f8c4acf7b0febea5432416
PhilippNaused <philipp.naused@t-online.de>
2025-08-04 09:16:50 +0200

    add missing type

---
+++ tests/Bshox.Tests/Polyfill.cs
@@ -0,0 +1,6 @@
+// ReSharper disable once CheckNamespace
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+internal sealed class ModuleInitializerAttribute : Attribute;
