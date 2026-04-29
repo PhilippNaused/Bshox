8982079c7dd1288c723e5e037b3d8c1ca91b41ab
Philipp Naused <32966607+PhilippNaused@users.noreply.github.com>
2026-04-29 20:48:17 +0200

    Add Author verification (#126)

---
+++ .github/workflows/authors.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+
+# Checks all commits on the current branch for AI-generated commits and reports a GitHub action error if any are found.
+
+echo "ref: $GITHUB_SHA"
+echo "base ref: $GITHUB_BASE_REF"
+
+range="origin/$GITHUB_BASE_REF..$GITHUB_SHA"
+
+echo "Checking commits:"
+git log --pretty=reference $range
+
+authors=$(git log --pretty='%ae' $range | sort -u)
+coAuthors=$(git log --pretty='format:%B' --grep='Co-authored-by:' $range | grep -i 'Co-authored-by:' | sed -E 's/.* <([^>]+)>/\1/' | sort -u)
+allAuthors=$(echo -e "$authors\n$coAuthors" | sort -u)
+echo Authors:
+echo "$allAuthors"
+knownClankers="noreply@anthropic\.com|copilot@github\.com|gemini@gogle\.cc" # TODO: Update this list as needed
+clankers=$(echo "$allAuthors" | grep -Ei "$knownClankers")
+if [ -n "$clankers" ]; then
+    echo "::error title=Clankers detected::Clankers detected in recent commits: $clankers"
+    exit 1
+fi
