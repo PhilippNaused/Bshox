8c88fd7a2a9d1b1bfce56bec03278cb29646e216
Philipp Naused <32966607+PhilippNaused@users.noreply.github.com>
2026-04-30 21:10:56 +0200

    Fix Async Test (#129)
    * Update readme link

--- .github/workflows/authors.sh
+++ .github/workflows/authors.sh
@@ -8,14 +8,13 @@ echo "base ref: $GITHUB_BASE_REF"
 range="origin/$GITHUB_BASE_REF..$GITHUB_SHA"
 
 echo "Checking commits:"
-git log --pretty=reference $range
+git --no-pager log --oneline $range
 
-authors=$(git log --pretty='%ae' $range | sort -u)
-coAuthors=$(git log --pretty='format:%B' --grep='Co-authored-by:' $range | grep -i 'Co-authored-by:' | sed -E 's/.* <([^>]+)>/\1/' | sort -u)
+authors=$(git log --pretty='%an <%ae>' $range | sort -u)
+coAuthors=$(git log --pretty='format:%B' --grep='Co-authored-by:' $range | grep -i 'Co-authored-by:' | sed -E 's/Co-authored-by: (.+)/\1/' | sort -u)
 allAuthors=$(echo -e "$authors\n$coAuthors" | sort -u)
-echo Authors:
-echo "$allAuthors"
-knownClankers="noreply@anthropic\.com|copilot@github\.com|gemini@gogle\.cc" # TODO: Update this list as needed
+echo -e "Authors:\n$allAuthors"
+knownClankers="noreply@anthropic\.com|copilot@github\.com|gemini@gogle\.cc|noreply@openai\.com" # TODO: Update this list as needed
 clankers=$(echo "$allAuthors" | grep -Ei "$knownClankers")
 if [ -n "$clankers" ]; then
     echo "::error title=Clankers detected::Clankers detected in recent commits: $clankers"
